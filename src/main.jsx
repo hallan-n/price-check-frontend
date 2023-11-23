25471caaacadeb6095972dcbc4c4aae9b2fc746f
hallan-n <hallanneves01@gmail.com>
2023-11-23 01:18:44 -0300

    Add router

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import App from './pages/Home'
+import App from './App'
 import './input.css'
 
 ReactDOM.createRoot(document.getElementById('root')).render(
--- src/pages/components/LoginForm.jsx
+++ src/pages/components/LoginForm.jsx
@@ -2,55 +2,54 @@ import { useState } from 'react'
 import axios from 'axios';
 
 function LoginForm() {
-
-
-    const [username, setUserName] = useState()
-    const [password, setPassword] = useState()
-
+    const [dadosLogin, setDadosLogin] = useState({
+        email: '',
+        password: '',
+    });
+    const handleChange = (event) => {
+        setDadosLogin({
+            ...dadosLogin,
+            [event.target.name]: event.target.value,
+        });
+    };
     const handleSubmit = async (event) => {
-        formData = {
-            username: username,
-            password: password,
-        }
+        event.preventDefault();
         try {
-            const resposta = await axios.post('http://localhost:8000/auth', formData);
-            console.log('Resposta do servidor:', resposta.data);
-        } catch (erro) {
-            console.error('Erro ao enviar dados:', erro.message);
+            const response = await axios.post('http://127.0.0.1:8000/auth', dadosLogin, {
+                withCredentials: true,
+            });
+            localStorage.setItem('accessToken', response.data.access_token);
+        } catch (error) {
+            console.error('Erro:', error.message);
         }
     };
 
-
     return (
-        <>
+        <form onSubmit={handleSubmit}>
             <div className="flex flex-col w-96 gap-2">
                 <h1 className="text-white text-2xl mb-2 text-center font-bold">Login</h1>
                 <input
                     className="bg-slate-600 rounded-md p-4 text-white"
                     type="text"
-                    name=""
-                    id=""
-                    placeholder="User"
-                    onChange={(e) => setUserName(e.target.value)}
+                    name="email"
+                    value={dadosLogin.email}
+                    onChange={handleChange}
                 />
                 <input
                     className="bg-slate-600 rounded-md p-4 text-white"
                     type="password"
-                    name=""
-                    id=""
-                    placeholder="Senha"
-                    onChange={(e) => setPassword(e.target.value)}
+                    name="password" // Alteração do nome do campo para "password"
+                    value={dadosLogin.password}
+                    onChange={handleChange}
                 />
-
                 <button
                     className="bg-slate-800 rounded-md p-4 text-white hover:bg-slate-700 transition"
                     type="submit"
-                    onClick={handleSubmit}
                 >Entrar</button>
                 <a className="text-blue-600 text-center font-bold mt-2" href="#">Criar conta</a>
             </div>
-        </>
-    )
+        </form>
+    );
 }
 
 export default LoginForm
